8a45cedb13a103e86633291a8c7a485733499e5a
Andrew Blankley <andrew.blankley@sap.com>
2024-06-20 15:54:47 +0100

    bug

--- test_oop.py
+++ test_oop.py
@@ -3,7 +3,6 @@ from oop import *
 def test_speak():
     miles = JackRussellTerrier("Miles", 4)
     danny =  Dachshund("Danny", 5)
-    assert(type(miles) == JackRussellTerrier.__class__)
     assert(miles.speak() == "Arf")
     assert(miles.speak("Grrr") == "Miles says Grrr")
     assert(miles.age == 4)
